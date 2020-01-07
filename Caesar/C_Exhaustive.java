2dc8a130282f745ef59e5dd5b7c07f8176c44ddb
DB-Hacks <Daniel.Brown.1396@gmail.com>
2020-01-07 17:44:25 -0500

    Updated C_Exhaustive.java
    Made a few error corrections and added proper documentation

--- Caesar/C_Exhaustive.java
+++ Caesar/C_Exhaustive.java
@@ -1,35 +1,46 @@
-package foundation;
+package Caesar;
 
-import util.CryptoTools;
+import Utility.CryptoTools;
 
+//An exhaustive attack on the Caesar cipher
 public class C_Exhaustive {
 	public static void main(String[] args) throws Exception
 	{
 		double dot = 0;
-		byte[] ct = CryptoTools.fileToBytes("C:\\Users\\Danie\\eclipse-workspace\\Labs\\EECS3481\\src\\data\\MSG2.ct");
-
+		int key = 0;
+		double highest = 0;
+		byte[] ct = CryptoTools.fileToBytes("Data/CT_MSG.txt");		//Test Case: NWNPXUNZZRENGGNPXVFZHPUZBERQVSSVPHYGGBQRGRPGORPNHFRBSGURSCTNFQVERPGNPPRFFGBFLFGRZERFBHEPRF ...
 		byte[] bk = new byte[ct.length];
-		for (int i = 1; i < 26; i++) {
+		
+		//Loop through every possible key
+		for (int i = 1; i < 26; i++) 
+		{
 			for (int j = 0; j < ct.length; j++)
 			{
-				int tmp = (ct[j] - 'A' - i) % 26;
+				int tmp = (ct[j] - 'A' - i) % 26;	//Shift each letter's position in the alphabet by the value of key 'i'
 				if (tmp < 0) tmp+= 26;
 				bk[j] = (byte) (tmp + 'A');
 			}
 			int [] freq = CryptoTools.getFrequencies(bk);
-			for(int k = 0; k < freq.length; k++) {
-				dot += ((freq[k])*(CryptoTools.ENGLISH[k]));
+			for(int k = 0; k < freq.length; k++) 
+			{
+				dot += ((freq[k])*(CryptoTools.ENGLISH[k]));	//calculate the dot product of the decrypted message frequency with the English language frequency
 			}
-			System.out.println("BK[" + i + "] = " + new String(bk));
-			System.out.println("Dot product [" + i + "] = " + dot);
+			if	(dot > highest)	{ 	//Highest dot product is the correct decryption, hence the highest value is being stored
+				highest = dot;
+				key = i;
+			}
 			dot = 0;
 		}
 		
-		//---------------------------------------------- Print
-		System.out.println("CT = " + new String(ct));
-		System.out.println("BK = " + new String(bk));
-		
-		//Look for the highest dot product to find the key
-		//first word is "COURSE" key is 22
+		//---------------------------------------------- Decrypt
+		for (int i = 0; i < ct.length; i++)
+		{
+			int tmp = (ct[i] - 'A' - key) % 26;
+			if (tmp < 0) tmp+= 26;		
+			bk[i] = (byte) (tmp + 'A');
+		}	
+		System.out.println("BK = " + new String(bk)); 	//Test Case: AJACKHAMMERATTACKISMUCHMOREDIFFICULTTODETECTBECAUSEOFTHEFPGASDIRECTACCESSTOSYSTEMRESOURCES ...
+		System.out.println("Key = " + key);		//Test Case: 13
 	}
 }
