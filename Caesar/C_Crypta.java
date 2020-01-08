92a003fe9c3240d1a80a00262fef4e52b6ec5a26
DB-Hacks <Daniel.Brown.1396@gmail.com>
2020-01-08 15:37:30 -0500

    Updated C_Crypta.java
    Fixed a few errors and added extra features increasing the program's efficiency

--- Caesar/C_Crypta.java
+++ Caesar/C_Crypta.java
@@ -1,6 +1,6 @@
-package foundation;
+package Caesar;
 
-import util.CryptoTools;
+import Utility.CryptoTools;
 
 public class C_Crypta {
 	public static void main(String[] args) throws Exception
@@ -8,12 +8,26 @@ public class C_Crypta {
 		byte[] ct = CryptoTools.fileToBytes("C:\\Users\\Danie\\eclipse-workspace\\Labs\\EECS3481\\src\\data\\MSG2.ct");
 		int[] freq = CryptoTools.getFrequencies(ct);
 		double letr = 0;
+		double highest = 0;
+		int positionE = 0;
+		int key = 0;
 		for(int i = 0; i < freq.length; i++) {
-			letr = (double) (freq[i])/(ct.length);
-			System.out.println("key "+ (i+1) +":" + letr*100);
+			letr = (double) (freq[i])/(ct.length); 	//Calculate the frequency of letters throughout the cipher
+			if	(letr > highest) {		//Track the most frequent letter is most likely to be the letter 'E'
+				highest = letr;
+				positionE = i + 1;
+			}
 		}
-		//find E highest number than subtract or add to get the shift
-		//in this case we have to add 4 to shift it to E or subtract 22
-		//when they originally did it they shifted it by 22
+		key = 5 - positionE;
+		if	(key < 0)
+			key = -key;
+		
+		System.out.println("Key Option 1: " + key); 	//Test Case: Key Option 1: 4
+		
+		key = positionE - 5;
+		if	(key < 0)
+			key = key + 26;
+		
+		System.out.println("Key Option 2: " + key);		//Test Case: Key Option 2: 22
 	}
 }
