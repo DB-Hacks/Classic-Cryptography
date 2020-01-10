0950efb583efefc5a59bb7870963809f47a75aeb
DB-Hacks <Daniel.Brown.1396@gmail.com>
2020-01-09 22:26:59 -0500

    Updated V_Crypta.java and deleted  VigDecrypt.java
    Removed VigDecrypt and implement its features into V_Crypta

--- Vigenere/V_Crypta.java
+++ Vigenere/V_Crypta.java
@@ -1,22 +1,29 @@
-package foundation;
+package Vigenere;
 
+import Utility.CryptoTools;
 import java.io.ByteArrayOutputStream;
-
-import util.CryptoTools;
 
 public class V_Crypta {
 	public static void main(String[] args) throws Exception
 	{
-		byte[] ct = CryptoTools.fileToBytes("C:\\Users\\Danie\\eclipse-workspace\\Labs\\EECS3481\\src\\data\\MSGV.ct");
+		byte[] ct = CryptoTools.fileToBytes("Data/CT_MSG7.txt");	//Test Case: PLBPFRTHVCEHFQYOBGIZFBYVYWIXPRNWEELETLRUWTUEEVERBBJFHVQLTJCE ...
 		for (int k = 1; k < 50; k++)
 		{
 			byte[] sample = sample(ct, k);
 			double ic = CryptoTools.getIC(sample, 10000);
 			System.out.printf("Key length %2d --> IC = %.3f\n", k, ic);
 		}
-		
-		int keyLength = 9;//find the key length using the above for loop
+		/*keyLength is found using the loop above, search for a repeating pattern to find the length
+		 Test Case:
+		 Key length  9 --> IC = 0.067
+		 Key length 18 --> IC = 0.063
+		 Key length 27 --> IC = 0.071
+		 Key length 36 --> IC = 0.070
+		 Key length 45 --> IC = 0.067
+		 */
+		int keyLength = 9;
         byte[] pt = new byte[ct.length];
+        byte[] key = new byte[keyLength];
         for(int i =0; i < keyLength; i++) {
             int[] test = new int[26];
             for(int j =i; j < ct.length ; j = j+keyLength) {
@@ -31,12 +38,15 @@ public class V_Crypta {
             	}
             }
             int shift = (maxIn - 4 + 26) % 26;
-            System.out.println( i + " key " + shift);
+            if(i < keyLength)
+            	key[i] = (byte) (shift + 'A');
             for(int j =i; j < ct.length ; j = j+keyLength) {
                 pt[j] = (byte)((ct[j] - 'A' - shift +26) %26 + 'A');
             }
         }
-        System.out.println(new String(pt));
+        
+        System.out.println("Key = " + new String(key));		//Test Case: ELABORATE
+        System.out.println("PT = " + new String(pt));		//Test Case: LABORATORYTHECHOICEOFAKEYDETERMINESAPARTICULARTRANSFORMATION ...
 	}
 	
 	public static byte[] sample(byte[] ar, int skip)
@@ -49,7 +59,4 @@ public class V_Crypta {
 		}
 		return baos.toByteArray();
 	}
-	
-	//Activity A first word LABORATORY
-	//Rehearsal first word The
 }
